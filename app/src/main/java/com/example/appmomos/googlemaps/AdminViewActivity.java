be07a449caaf04539a0440950c448cf76f31f6ec
vj <prachi@gmail.com>
2018-07-03 17:14:54 +0530

    get current location and view app map indications view done

--- app/src/main/java/com/example/appmomos/googlemaps/AdminViewActivity.java
+++ app/src/main/java/com/example/appmomos/googlemaps/AdminViewActivity.java
@@ -1,18 +1,17 @@
 package com.example.appmomos.googlemaps;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.Toast;
-
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
@@ -24,21 +23,22 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MapsActivity extends FragmentActivity implements
+public   class AdminViewActivity extends AppCompatActivity implements
         OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         GoogleMap.OnMarkerDragListener,
-        GoogleMap.OnMapLongClickListener,
-        View.OnClickListener
+        GoogleMap.OnMapLongClickListener
 {
 
-    @BindView(R.id.buttonView)
-    ImageButton buttonView;
-
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     //Our Map
     private GoogleMap mMap;
@@ -47,24 +47,24 @@ public class MapsActivity extends FragmentActivity implements
     private double longitude;
     private double latitude;
 
-    //Buttons
-    private ImageButton buttonCurrent;
 
     //Google ApiClient
     private GoogleApiClient googleApiClient;
 
 
-    //private GPSTracker mytracker;
-    public final static int TAG_PERMISSION_CODE=1;
-
-
-
-
+    @SuppressLint("NewApi")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_admin_view);
+
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Admin Section");
+
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -78,21 +78,34 @@ public class MapsActivity extends FragmentActivity implements
                 .addApi(LocationServices.API)
                 .build();
 
-        //Initializing views and adding onclick listeners
-        buttonCurrent = findViewById(R.id.buttonCurrent);
-        buttonCurrent.setOnClickListener(this);
-
-
-        buttonView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MapsActivity.this,AdminViewActivity.class);
-                startActivity(i);
-                finish();
-            }
-        });
-
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.blank_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == android.R.id.home)
+        {
+            startActivity(new Intent(AdminViewActivity.this, MapsActivity.class));
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        startActivity(new Intent(AdminViewActivity.this, MapsActivity.class));
+        finish();
+    }
+
 
 
     @Override
@@ -117,14 +130,6 @@ public class MapsActivity extends FragmentActivity implements
     //Getting current location
     private void getCurrentLocation()
     {
-
-        if(!CheckPermission.checkPermission(MapsActivity.this))
-        {
-            CheckPermission.requestPermission(MapsActivity.this,TAG_PERMISSION_CODE);
-            getCurrentLocation();
-        }
-        else
-        {
             mMap.clear();
 
             //Creating a location object
@@ -145,47 +150,66 @@ public class MapsActivity extends FragmentActivity implements
                 latitude = location.getLatitude();
 
                 //moving the map to location
-                moveMap();
-
-               // moveMap1(latitude,longitude);
+                 moveMap1(latitude,longitude);
             }
+    }
+
+    private void moveMap1(double latitude, double longitude)
+    {
+      LatLng latLng0 = new LatLng(latitude, longitude);
+
+        ArrayList<Double> latitudeArray = new ArrayList<>();
+        latitudeArray.add(latitude);
+        latitudeArray.add(latitude+1);
+        latitudeArray.add(latitude+0.2);
+        latitudeArray.add(latitude+0.5);
+        latitudeArray.add(latitude-0.5);
+
+
+        ArrayList<Double> longitudeArray = new ArrayList<>();
+        longitudeArray.add(longitude);
+        longitudeArray.add(longitude+1);
+        longitudeArray.add(longitude+0.2);
+        longitudeArray.add(longitude+0.5);
+        longitudeArray.add(longitude-0.5);
+
+        int zoomCapacity = 8;
+
+        for (int i =0;i<5;i++)
+        {
+            LatLng latLng = new LatLng(latitudeArray.get(i), longitudeArray.get(i));
+
+            mMap.addMarker(new MarkerOptions()
+                    .position(latLng)
+                    .draggable(false)
+                    .title("Emp "+(i+1) ));
         }
 
-    }
+        /*mMap.addMarker(new MarkerOptions()
+                .position(latLng1)
+                .draggable(false)
+                .title("Emp 2"));
 
-    //Function to move the map
-    private void moveMap() {
-        //String to display current latitude and longitude
-        String msg = latitude + ", "+longitude;
-
-        //Creating a LatLng Object to store Coordinates
-        LatLng latLng = new LatLng(latitude, longitude);
-
-        //Adding marker to map
         mMap.addMarker(new MarkerOptions()
-                .position(latLng) //setting position
-                .draggable(true) //Making the marker draggable
-                .title("Current Location")); //Adding a title
+                .position(latLng2)
+                .draggable(false)
+                .title("Emp 3"));
 
-        //Moving the camera
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
-        //Animating the camera
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng3)
+                .draggable(false)
+                .title("Emp 4"));
 
-        //Displaying current coordinates in toast
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng4)
+                .draggable(false)
+                .title("Emp 5"));*/
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng0));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(zoomCapacity));
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        LatLng latLng = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(latLng).draggable(true));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.setOnMarkerDragListener(this);
-        mMap.setOnMapLongClickListener(this);
-    }
 
     @Override
     public void onConnected(Bundle bundle) {
@@ -210,7 +234,7 @@ public class MapsActivity extends FragmentActivity implements
         //Adding a new marker to the current pressed position
         mMap.addMarker(new MarkerOptions()
                 .position(latLng)
-                .draggable(true));
+                .draggable(false));// false means not dragable and true means map icon draggable
     }
 
     @Override
@@ -228,40 +252,18 @@ public class MapsActivity extends FragmentActivity implements
         //Getting the coordinates
         latitude = marker.getPosition().latitude;
         longitude = marker.getPosition().longitude;
-
-        //Moving the map
-      //  moveMap();
     }
+
 
     @Override
-    public void onClick(View v) {
-        if(v == buttonCurrent){
-           getCurrentLocation();
-           // moveMap();
-        }
-    }
-}
-
-
-class CheckPermission {
-
-    //  CHECK FOR LOCATION PERMISSION
-    public static boolean checkPermission(MapsActivity activity){
-        int result = ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION);
-        return result == PackageManager.PERMISSION_GRANTED;
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng latLng = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(latLng).draggable(true));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.setOnMarkerDragListener(this);
+        mMap.setOnMapLongClickListener(this);
     }
 
-    //REQUEST FOR PERMISSSION
-    public static void requestPermission(MapsActivity activity, final int code)
-    {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(activity,Manifest.permission.ACCESS_FINE_LOCATION)){
-
-            Toast.makeText(activity,"GPS permission allows us to access location data. Please allow in App Settings for additional functionality.",Toast.LENGTH_LONG).show();
-
-        } else {
-
-            ActivityCompat.requestPermissions(activity,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},code);
-        }
-    }
 
 }
